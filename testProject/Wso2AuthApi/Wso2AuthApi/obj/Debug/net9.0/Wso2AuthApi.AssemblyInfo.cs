c4f5fc40331912b1902ce2b4036977b864116e3a
NAZMUL KARIM TANVIR <nazmul.karim.tanvir@g.bracu.ac.bd>
2025-08-27 10:16:23 +0600

    installed tailwind

--- testProject/Wso2AuthApi/Wso2AuthApi/obj/Debug/net9.0/Wso2AuthApi.AssemblyInfo.cs
+++ testProject/Wso2AuthApi/Wso2AuthApi/obj/Debug/net9.0/Wso2AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6636117e03f4dd0576c7f59bc6f8c375e6b3263d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d458e6d0fe69a8ce500c254202923cb600038d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
