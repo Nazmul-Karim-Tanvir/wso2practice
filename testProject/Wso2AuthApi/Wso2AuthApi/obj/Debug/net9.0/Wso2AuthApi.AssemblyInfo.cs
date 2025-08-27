10ff6697ad3711935b127db213d99b06c174c9ab
NAZMUL KARIM TANVIR <nazmul.karim.tanvir@g.bracu.ac.bd>
2025-08-27 15:57:07 +0600

    trying to fix git ignore issue

--- testProject/Wso2AuthApi/Wso2AuthApi/obj/Debug/net9.0/Wso2AuthApi.AssemblyInfo.cs
+++ testProject/Wso2AuthApi/Wso2AuthApi/obj/Debug/net9.0/Wso2AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a162d4b9112df231aa0c43f687e228fff30a36b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76a0f0fda6c4a1b5ea168e72585935b8f17d200")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wso2AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
